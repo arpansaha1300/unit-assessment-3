2f0be5786b453380768944e8891bf3a73fcbbeaa
arpansaha1300 <Arpan.Saha3@amdocs.com>
2024-07-30 16:38:58 +0530

    alert drawer

---
+++ src/components/common/Drawer.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+} from '@headlessui/react'
+import {
+  ArrowRightIcon,
+  ArrowTopRightOnSquareIcon,
+} from '@heroicons/react/24/outline'
+
+interface DrawerProps {
+  open: boolean
+  title: string
+  children: React.ReactNode
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
+
+export default function Drawer(props: Readonly<DrawerProps>) {
+  const { open, title, children, setOpen } = props
+
+  return (
+    <Dialog open={open} onClose={setOpen} className="relative z-10">
+      <DialogBackdrop
+        transition
+        className="fixed inset-0 bg-gray-700/50 transition-opacity duration-500 ease-in-out data-[closed]:opacity-0"
+      />
+
+      <div className="fixed inset-0 overflow-hidden">
+        <div className="absolute inset-0 overflow-hidden">
+          <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+            <DialogPanel
+              transition
+              className="pointer-events-auto relative w-screen max-w-md transform transition duration-500 ease-in-out data-[closed]:translate-x-full sm:duration-700"
+            >
+              <div className="flex h-full flex-col overflow-y-auto bg-white py-6 shadow-xl">
+                <div className="px-4 sm:px-6 flex items-center justify-between">
+                  <div className="flex items-center gap-3">
+                    <DialogTitle className="text-lg font-semibold leading-6 text-gray-900">
+                      {title}
+                    </DialogTitle>
+                    <ArrowTopRightOnSquareIcon className="flex-shrink-0 size-5 text-gray-600" />
+                  </div>
+                  <button
+                    type="button"
+                    onClick={() => setOpen(false)}
+                    className="rounded size-6 flex items-center justify-center text-gray-800 focus:outline-none focus:ring-2 focus:ring-brand-primary"
+                  >
+                    <span className="sr-only">Close panel</span>
+                    <ArrowRightIcon aria-hidden="true" className="size-5" />
+                  </button>
+                </div>
+                <div className="relative mt-6 flex-1 px-4 sm:px-6">
+                  {children}
+                </div>
+              </div>
+            </DialogPanel>
+          </div>
+        </div>
+      </div>
+    </Dialog>
+  )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,16 @@
 import { BellIcon } from '@heroicons/react/24/solid'
 import Breadcrumb from '~/components/common/Breadcrumb'
+import Drawer from '~common/Drawer'
 import DataUsage from '~/components/internet-of-things/DataUsage'
 import Overview from '~/components/internet-of-things/Overview'
 import Reports from '~/components/internet-of-things/Reports'
 import TopUsage from '~/components/internet-of-things/TopUsage'
+import { useState } from 'react'
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from '@headlessui/react'
 
 export function Component() {
+  const [drawerOpen, setDrawerOpen] = useState(false)
+
   const breadcrumbItems = [
     {
       name: 'Home',
@@ -16,6 +21,7 @@ export function Component() {
       path: '/internet-of-things',
     },
   ]
+
   return (
     <div>
       <Breadcrumb items={breadcrumbItems} />
@@ -25,13 +31,18 @@ export function Component() {
 
         <div className="flex items-center gap-6">
           <p className="text-sm font-semibold">Account number: 1234567899</p>
-          <button className="px-2 py-1.5 text-sm font-semibold border-2 border-gray-950 inline-flex items-center gap-1 rounded">
+          <button
+            className="px-2 py-1.5 text-sm font-semibold border-2 border-gray-950 hover:bg-gray-100 inline-flex items-center gap-1 rounded transition-colors"
+            onClick={() => setDrawerOpen(true)}
+          >
             <div className="relative">
               <BellIcon className="flex-shrink-0 size-5" />
               <span className="size-2.5 bg-brand-primary rounded-full absolute top-0 right-0 -translate-y-1/2" />
             </div>
             <p>Alerts</p>
           </button>
+
+          <AlertsDrawer open={drawerOpen} setOpen={setDrawerOpen} />
         </div>
       </div>
 
@@ -53,3 +64,80 @@ export function Component() {
 }
 
 Component.displayName = 'Home'
+
+function AlertsDrawer(props) {
+  const { open, setOpen } = props
+  // const [selectedIdx, setSelectedIdx] = useState(0)
+
+  return (
+    <Drawer open={open} setOpen={setOpen} title="Alerts">
+      <TabGroup defaultIndex={0}>
+        <TabList className="grid grid-cols-2 text-sm font-semibold">
+          {alertTabs.map(tab => (
+            <Tab key={tab.name} className="group py-2.5 relative">
+              <p>{tab.name}</p>
+              <span className="hidden group-data-[selected]:block h-1 bg-brand-primary absolute bottom-0 inset-x-0" />
+            </Tab>
+          ))}
+        </TabList>
+
+        <TabPanels className="py-2.5">
+          <TabPanel>
+            <ul className="divide-y divide-gray-300">
+              {newAlerts.map(alert => (
+                <li key={alert.id} className="pl-8 py-3 text-xs relative">
+                  <p className="font-semibold">{alert.title}</p>
+                  <p className="text-gray-600">{alert.date}</p>
+                  <p className="mt-1 text-gray-600">{alert.description}</p>
+                  <p className="mt-1 font-light text-gray-500">ID {alert.id}</p>
+                  <span className="size-2 bg-blue-500 rounded-full absolute top-5 left-4 -translate-x-1/2" />
+                </li>
+              ))}
+            </ul>
+          </TabPanel>
+
+          <TabPanel>Blank</TabPanel>
+        </TabPanels>
+      </TabGroup>
+    </Drawer>
+  )
+}
+
+const alertTabs = [
+  {
+    name: 'New alerts',
+  },
+  {
+    name: 'Alert logs',
+  },
+]
+const newAlerts = [
+  {
+    id: '001',
+    title: 'System Update',
+    description:
+      'A scheduled system update will occur tonight between 2 AM and 4 AM. During this time, the service may be temporarily unavailable.',
+    date: '2024-07-30',
+  },
+  {
+    id: '002',
+    title: 'Security Alert',
+    description:
+      'Unusual login activity has been detected on your account. Please review recent login attempts and change your password if necessary.',
+    date: '2024-07-29',
+  },
+  {
+    id: '003',
+    title: 'Maintenance Window',
+    description:
+      'Planned maintenance on the payment gateway will be carried out from 1 AM to 3 AM. Please complete any transactions before this time.',
+    date: '2024-07-31',
+  },
+  {
+    id: '004',
+    title: 'New Feature Release',
+    description:
+      'We are excited to announce a new feature in our app: real-time notifications! Update your app to experience the new functionality.',
+    date: '2024-07-28',
+  },
+]
